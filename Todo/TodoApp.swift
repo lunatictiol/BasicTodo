b2d22030143e238c04d0dc27b3cd5d4c3cc1e76a
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-04 12:20:47 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by sabiq on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
