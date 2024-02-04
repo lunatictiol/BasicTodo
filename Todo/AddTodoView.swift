3c3bdb9c5f37d66a97e9f12afbd11e6d254007f0
sabiq aijaz <bhatsabiq9@gmail.com>
2024-02-04 15:48:14 +0530

    first commit

---
+++ Todo/AddTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTodoView.swift
+//  Todo
+//
+//  Created by sabiq on 04/02/24.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTodoView()
+}
